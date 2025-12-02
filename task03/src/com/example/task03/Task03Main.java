163a3a81e18238957d72718b76d3f29583c4860e
EgorS45 <EgorS45@users.noreply.github.com>
2025-12-02 10:30:10 +0500

    Add IO2 task solutions

--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -2,6 +2,7 @@ package com.example.task03;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.ObjectInputStream;
 
 public class Task03Main {
     public static void main(String[] args) throws IOException, ClassNotFoundException {
@@ -15,7 +16,12 @@ public class Task03Main {
     }
 
     public static SampleData deserialize(InputStream inputStream) throws IOException, ClassNotFoundException {
-        // your implementation here
-        return null;
+        try (ObjectInputStream objectInputStream = new ObjectInputStream(inputStream)) {
+            Object object = objectInputStream.readObject();
+            if (object instanceof SampleData) {
+                return (SampleData) object;
+            }
+            throw new ClassNotFoundException("Unexpected data type: " + object.getClass().getName());
+        }
     }
 }
